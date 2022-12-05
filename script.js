451357c4f87f1b1ce0d4fded6fbde24ce4ea7115
AmandaSolee <79324616+AmandaSolee@users.noreply.github.com>
2022-12-05 13:25:46 -0500

    finalizing filters & fixing errors

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ queryInputElem.addEventListener('keyup', async function(ev) {
       createInstructions();
 
       for (let i = 0; i < 10; i++) {
-        let drinkElem = await createDrinkElem(searchResultsJson.drinks[i]);
-        resultsElem.append(drinkElem);
+        if (searchResultsJson.drinks[i] != null) {
+          let drinkElem = await createDrinkElem(searchResultsJson.drinks[i], 0);
+          resultsElem.append(drinkElem);
+        }
       }
     }
 });
 
-async function createDrinkElem(drink) {
+async function createDrinkElem(drink, filter) {
   let drinkElem = document.createElement('div');
   let drinkName = document.createElement('h2');
   let ingredientList = document.createElement('ol');
@@ -32,7 +34,6 @@ async function createDrinkElem(drink) {
   let ingredient2 = document.createElement('li');
   let ingredient3 = document.createElement('li');
   let ingredient4 = document.createElement('li');
-  let ingredient5 = document.createElement('li');
   let recipe = document.createElement('div');
 
   drinkElem.appendChild(drinkName);
@@ -45,20 +46,45 @@ async function createDrinkElem(drink) {
   ingredient2.append(drink.strIngredient2);
   ingredient3.append(drink.strIngredient3);
   ingredient4.append(drink.strIngredient4);
-  ingredient5.append(drink.strIngredient5);
   recipe.append(drink.strInstructions);
 
   ingredientList.appendChild(ingredient1);
   ingredientList.appendChild(ingredient2);
   ingredientList.appendChild(ingredient3);
   ingredientList.appendChild(ingredient4);
-  ingredientList.appendChild(ingredient5);
 
   createIngredientFacts(ingredient1, drink.strIngredient1);
   createIngredientFacts(ingredient2, drink.strIngredient2);
   createIngredientFacts(ingredient3, drink.strIngredient3);
   createIngredientFacts(ingredient4, drink.strIngredient4);
-  createIngredientFacts(ingredient5, drink.strIngredient5);
+
+  if (filter == 0) { // no filter selected (or show all results)
+    let ingredient5 = document.createElement('li');
+    ingredient5.append(drink.strIngredient5);
+    ingredientList.appendChild(ingredient5);
+    createIngredientFacts(ingredient5, drink.strIngredient5);
+
+  } else if (filter == 1) { // filter selected: >= five ingredients
+    let ingredient5 = document.createElement('li');
+    let ingredient6 = document.createElement('li');
+    let ingredient7 = document.createElement('li');
+    let ingredient8 = document.createElement('li');
+
+    ingredient5.append(drink.strIngredient5);
+    ingredient6.append(drink.strIngredient6);
+    ingredient7.append(drink.strIngredient7);
+    ingredient8.append(drink.strIngredient8);
+
+    ingredientList.appendChild(ingredient5);
+    ingredientList.appendChild(ingredient6);
+    ingredientList.appendChild(ingredient7);
+    ingredientList.appendChild(ingredient8);
+
+    createIngredientFacts(ingredient5, drink.strIngredient5);
+    createIngredientFacts(ingredient6, drink.strIngredient6);
+    createIngredientFacts(ingredient7, drink.strIngredient7);
+    createIngredientFacts(ingredient8, drink.strIngredient8);
+  }
 
   return drinkElem;
 }
@@ -115,19 +141,24 @@ function createFilterButtons() {
   let filters = document.createElement('div');
   let less5 = document.createElement('button');
   let greater5 = document.createElement('button');
+  let allResults = document.createElement('button');
 
   less5.type = "button";
   greater5.type = "button";
+  allResults.type = "button";
 
   filters.append('Options to filter results: ');
   less5.append('less than 5 ingredients');
   greater5.append('greater than 5 ingredients');
+  allResults.append('all results');
 
   less5.classList.add('filter');
   greater5.classList.add('filter');
+  allResults.classList.add('filter');
 
   filters.append(less5);
   filters.append(greater5);
+  filters.append(allResults);
   resultsElem.append(filters);
 
   less5.addEventListener('click', async function(ev) {
@@ -141,8 +172,8 @@ function createFilterButtons() {
     console.log(searchResultsJson);
 
     for (let i = 0; i < 10; i++) {
-      if (searchResultsJson.drinks[i].strIngredient5 == null) {
-        let drinkElem = await createDrinkElem(searchResultsJson.drinks[i]);
+      if (searchResultsJson.drinks[i] != null && searchResultsJson.drinks[i].strIngredient5 == null) {
+        let drinkElem = await createDrinkElem(searchResultsJson.drinks[i], -1);
         resultsElem.append(drinkElem);
       }
     }
@@ -159,8 +190,26 @@ function createFilterButtons() {
     console.log(searchResultsJson);
 
     for (let i = 0; i < 10; i++) {
-      if (searchResultsJson.drinks[i].strIngredient5 != null) {
-        let drinkElem = await createDrinkElem(searchResultsJson.drinks[i]);
+      if (searchResultsJson.drinks[i] != null && searchResultsJson.drinks[i].strIngredient5 != null) {
+        let drinkElem = await createDrinkElem(searchResultsJson.drinks[i], 1);
+        resultsElem.append(drinkElem);
+      }
+    }
+  });
+
+  allResults.addEventListener('click', async function(ev) {
+    clearResultsElem();
+    createFilterButtons();
+    createInstructions();
+
+    const searchResults = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${queryInputElem.value}`);
+    const searchResultsJson = await searchResults.json();
+    console.log(searchResultsJson);
+
+    for (let i = 0; i < 10; i++) {
+      if (searchResultsJson.drinks[i] != null) {
+        let drinkElem = await createDrinkElem(searchResultsJson.drinks[i], 0);
         resultsElem.append(drinkElem);
       }
     }
